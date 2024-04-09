052d00bbbbbd63e1acaf1aa39601143f94c6dd97
saepudinasep <saepudinasep2001@gmail.com>
2024-04-09 15:21:32 +0700

    [FEATURE] menambahkan table attendances

--- database/migrations/2024_04_09_071525_attendances.php
+++ database/migrations/2024_04_09_071525_attendances.php
@@ -12,6 +12,16 @@ return new class extends Migration
     public function up(): void
     {
         //
+        Schema::create('attendances', function (Blueprint $table) {
+            $table->string('id')->primary();
+            $table->foreignId('user_id')->constrained('users');
+            $table->string('latitude', 20)->nullable();
+            $table->string('longitude', 20)->nullable();
+            $table->string('address', 500)->nullable();
+            $table->enum('status', ['attend', 'sick', 'leave', 'permit', 'business_trip', 'remote']);
+            $table->string('description', 500)->nullable();
+            $table->timestamps();
+        });
     }
 
     /**
@@ -20,5 +30,6 @@ return new class extends Migration
     public function down(): void
     {
         //
+        Schema::dropIfExists('attendances');
     }
 };
