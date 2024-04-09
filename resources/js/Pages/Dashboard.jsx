b5d1e1a75e205845d1aeb513ee66fce17c7e5b94
saepudinasep <saepudinasep2001@gmail.com>
2024-04-09 15:49:23 +0700

    [FEATURE] menambahkan component submitAttendances

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,5 +1,6 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Head } from '@inertiajs/react';
+import SubmitAttendance from '@/Components/Attendance/Submit';
 
 export default function Dashboard({ auth }) {
     return (
@@ -10,9 +11,17 @@ export default function Dashboard({ auth }) {
             <Head title="Dashboard" />
 
             <div className="py-12">
-                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                         <div className="p-6 text-gray-900">You're logged in!</div>
+                    </div>
+
+                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                        <section className="max-w-xl">
+                            <div className="p-6 text-gray-900">
+                                <SubmitAttendance />
+                            </div>
+                        </section>
                     </div>
                 </div>
             </div>
